103d8496866fd9d2ab0d64a7c8d347ab916e053f
datguy14 <locklearcharles000@gmail.com>
2026-02-22 19:26:31 -0500

    Refactor sendFax to use axios and Sinch API

--- controllers/faxController.js
+++ controllers/faxController.js
@@ -1,4 +1,5 @@
-const phaxio = require('../utils/twilioClient');
+const axios = require('axios');
+const { getAccessToken, SINCH_PROJECT_ID } = require('../utils/twilioClient');
 
 exports.sendFax = async (req, res) => {
   try {
@@ -8,14 +9,34 @@ exports.sendFax = async (req, res) => {
       return res.status(400).json({ error: "Missing 'to' or 'fileUrl'" });
     }
 
-    const fax = await phaxio.faxes.create({
-      to,
-      file_url: fileUrl
+    const accessToken = await getAccessToken();
+
+    const faxUrl = `https://fax.api.sinch.com/v1/projects/${SINCH_PROJECT_ID}/faxes`;
+
+    const response = await axios.post(
+      faxUrl,
+      {
+        to,
+        fileUrl
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    res.json({
+      success: true,
+      faxId: response.data.id
     });
 
-    res.json({ success: true, faxId: fax.id });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Fax failed", details: error.message });
+    console.error(error.response?.data || error.message);
+    res.status(500).json({
+      error: "Fax failed",
+      details: error.response?.data || error.message
+    });
   }
 };
