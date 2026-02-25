9d20f463dc7be1d3c78fd358a70e392d3097ba61
datguy14 <locklearcharles000@gmail.com>
2026-02-24 23:59:45 -0500

    Modify fax sending logic to use updated API
    Updated the Sinch Fax API endpoint and added the 'from' field to the fax payload.

--- controllers/faxController.js
+++ controllers/faxController.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { getAccessToken, SINCH_PROJECT_ID } = require('../utils/twilioClient');
+const { getAccessToken, SINCH_PROJECT_ID, SINCH_FAX_NUMBER } = require('../utils/twilioClient');
 
 exports.sendFax = async (req, res) => {
   try {
@@ -13,27 +13,32 @@ exports.sendFax = async (req, res) => {
     const accessToken = await getAccessToken();
 
     // Correct Sinch Fax API v3 endpoint
-    const faxUrl = `https://fax.api.sinch.com/v1/projects/${SINCH_PROJECT_ID}/faxes:send`;
+    const faxUrl = `https://fax.api.sinch.com/v3/projects/${SINCH_PROJECT_ID}/faxes`;
+
+    // Build fax payload
+    const payload = {
+      from: SINCH_FAX_NUMBER,   // your Sinch fax number
+      to: to,
+      media: [
+        {
+          url: fileUrl
+        }
+      ]
+    };
 
     // Send fax request
-    const response = await axios.post(
-      faxUrl,
-      {
-        to,
-        fileUrl
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json'
-        }
+    const response = await axios.post(faxUrl, payload, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
       }
-    );
+    });
 
     // Success response
     res.json({
       success: true,
-      faxId: response.data.id || response.data.faxId || "unknown"
+      faxId: response.data.id || "unknown",
+      status: response.data.status || "submitted"
     });
 
   } catch (error) {
