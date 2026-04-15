3c8685a7ec6195a3bb67d53ae9a33c6d3344d309
datguy14 <locklearcharles000@gmail.com>
2026-04-15 10:56:08 -0400

    Implement fax status retrieval service

---
+++ src/services/src/services/faxStatusService.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+exports.getFaxStatus = async (faxId) => {
+  const url = `https://fax.api.sinch.com/v3/projects/${process.env.SINCH_PROJECT_ID}/faxes/${faxId}`;
+
+  const response = await axios.get(url, {
+    auth: {
+      username: process.env.SINCH_KEY_ID,
+      password: process.env.SINCH_KEY_SECRET
+    }
+  });
+
+  return response.data;
+};
