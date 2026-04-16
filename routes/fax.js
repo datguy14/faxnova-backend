3f83b36962f43e2e58116f2627d7e48aa9f0118d
datguy14 <locklearcharles000@gmail.com>
2026-04-16 01:03:18 -0400

    Delete routes/fax.js

--- routes/fax.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendFax } = require('../controllers/faxController');
-
-router.post('/send', sendFax);
-
-module.exports = router;
