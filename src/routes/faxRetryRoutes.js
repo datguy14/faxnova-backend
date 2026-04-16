8a150aa4282e3578cfa6fd2788513e7c72c6ec54
datguy14 <locklearcharles000@gmail.com>
2026-04-16 00:32:16 -0400

    Update fax retry route to remove '/retry' segment

--- src/routes/faxRetryRoutes.js
+++ src/routes/faxRetryRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { retryFaxController } = require('../controllers/faxRetryController');
 
-router.post('/:faxId/retry', retryFaxController);
+// POST /fax/retry/:faxId
+router.post('/:faxId', retryFaxController);
 
 module.exports = router;
