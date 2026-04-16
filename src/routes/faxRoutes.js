728aa9ccce2563bdb0d96de3031010fe7884b5cb
datguy14 <locklearcharles000@gmail.com>
2026-04-16 00:20:03 -0400

    Add faxRoutes for sending faxes

---
+++ src/routes/faxRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const { sendFax } = require('../controllers/faxController');
+const validateSendFax = require('../middleware/validateSendFax');
+
+// POST /fax/send
+router.post('/send', validateSendFax, sendFax);
+
+module.exports = router;
