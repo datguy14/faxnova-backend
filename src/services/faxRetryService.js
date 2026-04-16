b11e8c8ab4d680680a8897de7a97830a1c7b5e3b
datguy14 <locklearcharles000@gmail.com>
2026-04-16 00:26:57 -0400

    Update faxRetryService.js

--- src/services/faxRetryService.js
+++ src/services/faxRetryService.js
@@ -1,18 +1,32 @@
 const axios = require('axios');
 
-exports.retryFax = async (faxId) => {
-  const url = `https://fax.api.sinch.com/v3/projects/${process.env.SINCH_PROJECT_ID}/faxes/${faxId}/retry`;
+exports.retryFax = async (faxId, correlationId) => {
+  try {
+    const url = `https://fax.api.sinch.com/v3/projects/${process.env.SINCH_PROJECT_ID}/faxes/${faxId}/retry`;
 
-  const response = await axios.post(
-    url,
-    {},
-    {
-      auth: {
-        username: process.env.SINCH_KEY_ID,
-        password: process.env.SINCH_KEY_SECRET
+    const response = await axios.post(
+      url,
+      {},
+      {
+        auth: {
+          username: process.env.SINCH_KEY_ID,
+          password: process.env.SINCH_KEY_SECRET
+        },
+        timeout: 10000,
+        headers: {
+          'X-Correlation-ID': correlationId
+        }
       }
-    }
-  );
+    );
 
-  return response.data;
+    return response.data;
+
+  } catch (error) {
+    throw {
+      message: 'Failed to retry fax',
+      status: error.response?.status || 500,
+      details: error.response?.data || error.message,
+      correlationId
+    };
+  }
 };
