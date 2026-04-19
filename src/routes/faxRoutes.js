6fc4d9c080ca1dc9103a3fde1309bf358e839cf6
datguy14 <locklearcharles000@gmail.com>
2026-04-18 21:09:31 -0400

    refactor: clean faxRoutes — wire validateSendFax middleware

--- src/routes/faxRoutes.js
+++ src/routes/faxRoutes.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
-const { sendFax, getFaxStatus } = require('../controllers/faxController');
+const { sendFax } = require('../controllers/faxController');
 const validateSendFax = require('../middleware/validateSendFax');
 
 // POST /fax/send
 router.post('/send', validateSendFax, sendFax);
-
-// GET /fax/status/:faxId   ← ADD THIS RIGHT HERE
-router.get('/status/:faxId', getFaxStatus);
 
 module.exports = router;
