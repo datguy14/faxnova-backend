4b1759b956d772f7fab88ecd843495b7e7669973
datguy14 <locklearcharles000@gmail.com>
2026-04-18 21:09:32 -0400

    refactor: clean faxStatusRoutes

--- src/routes/faxStatusRoutes.js
+++ src/routes/faxStatusRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { checkFaxStatus } = require('../controllers/faxStatusController');
 const validateFaxId = require('../middleware/validateFaxId');
 
+// GET /fax/status/:faxId
 router.get('/:faxId', validateFaxId, checkFaxStatus);
 
 module.exports = router;
