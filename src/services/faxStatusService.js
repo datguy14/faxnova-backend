44ebe28ac8940f7886298a5fe845c4b2f41b70f2
datguy14 <locklearcharles000@gmail.com>
2026-05-15 00:25:26 -0400

    Update faxStatusService.js

--- src/services/faxStatusService.js
+++ src/services/faxStatusService.js
@@ -1,15 +1,30 @@
-const axios = require('axios');
+// src/services/faxStatusService.js
+const { sinchRequest } = require('../sinchAuth');
+
+function validateFaxId(faxId) {
+  return /^[a-zA-Z0-9-]{8,}$/.test(faxId);
+}
 
 exports.getFaxStatus = async (faxId) => {
+  if (!validateFaxId(faxId)) {
+    const err = new Error("Invalid faxId format");
+    err.status = 400;
+    throw err;
+  }
+
   const url = `https://fax.api.sinch.com/v3/projects/${process.env.SINCH_PROJECT_ID}/faxes/${faxId}`;
 
-  const response = await axios.get(url, {
-    auth: {
-      username: process.env.SINCH_KEY_ID,
-      password: process.env.SINCH_KEY_SECRET
-    },
-    timeout: 10000
+  const resp = await sinchRequest({
+    method: 'GET',
+    url
   });
 
-  return response.data;
+  if (resp.status >= 400) {
+    const err = new Error("Sinch getFaxStatus failed");
+    err.status = resp.status;
+    err.details = resp.data;
+    throw err;
+  }
+
+  return resp.data;
 };
