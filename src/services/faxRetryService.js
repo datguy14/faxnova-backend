8b9d996562d3b8a1136260016b5d3f6197dcc308
datguy14 <locklearcharles000@gmail.com>
2026-05-15 00:22:22 -0400

    Update faxRetryService.js

--- src/services/faxRetryService.js
+++ src/services/faxRetryService.js
@@ -1,32 +1,35 @@
-const axios = require('axios');
+// src/services/faxRetryService.js
+const { sinchRequest } = require('../sinchAuth');
+
+function validateFaxId(faxId) {
+  return /^[a-zA-Z0-9-]{8,}$/.test(faxId);
+}
 
 exports.retryFax = async (faxId, correlationId) => {
-  try {
-    const url = `https://fax.api.sinch.com/v3/projects/${process.env.SINCH_PROJECT_ID}/faxes/${faxId}/retry`;
-
-    const response = await axios.post(
-      url,
-      {},
-      {
-        auth: {
-          username: process.env.SINCH_KEY_ID,
-          password: process.env.SINCH_KEY_SECRET
-        },
-        timeout: 10000,
-        headers: {
-          'X-Correlation-ID': correlationId
-        }
-      }
-    );
-
-    return response.data;
-
-  } catch (error) {
-    throw {
-      message: 'Failed to retry fax',
-      status: error.response?.status || 500,
-      details: error.response?.data || error.message,
-      correlationId
-    };
+  if (!validateFaxId(faxId)) {
+    const err = new Error("Invalid faxId format");
+    err.status = 400;
+    throw err;
   }
+
+  const url = `https://fax.api.sinch.com/v3/projects/${process.env.SINCH_PROJECT_ID}/fax/${faxId}/retry`;
+
+  const resp = await sinchRequest({
+    method: 'POST',
+    url,
+    data: {},
+    headers: {
+      'X-Correlation-ID': correlationId
+    }
+  });
+
+  if (resp.status >= 400) {
+    const err = new Error("Sinch retryFax failed");
+    err.status = resp.status;
+    err.details = resp.data;
+    err.correlationId = correlationId;
+    throw err;
+  }
+
+  return resp.data;
 };
