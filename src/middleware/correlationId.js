011b6f71cd141600e620ab45f0175c024ae898b6
datguy14 <locklearcharles000@gmail.com>
2026-05-18 12:54:35 -0400

    Update correlationId.js

--- src/middleware/correlationId.js
+++ src/middleware/correlationId.js
@@ -1,7 +1,23 @@
-const { v4: uuidv4 } = require('uuid');
+// src/middleware/correlationId.js
 
-module.exports = (req, res, next) => {
-  req.correlationId = req.headers['x-correlation-id'] || uuidv4();
-  res.setHeader('X-Correlation-ID', req.correlationId);
+const { randomUUID } = require('crypto');
+
+/**
+ * Ensures every request has a correlation ID for tracing.
+ * - Respects incoming X-Correlation-Id header
+ * - Generates a UUID if missing
+ * - Attaches to req.correlationId
+ * - Echoes back in response header
+ */
+module.exports = function correlationId(req, res, next) {
+  const incoming = req.headers['x-correlation-id'];
+
+  const id = incoming && typeof incoming === 'string'
+    ? incoming.trim()
+    : randomUUID();
+
+  req.correlationId = id;
+  res.setHeader('X-Correlation-Id', id);
+
   next();
 };
