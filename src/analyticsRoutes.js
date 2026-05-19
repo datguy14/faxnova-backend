866a983d7279b5bccb757db38bb07592ea6356f5
datguy14 <locklearcharles000@gmail.com>
2026-05-18 21:38:46 -0400

    Create analyticsRoutes.js

---
+++ src/analyticsRoutes.js
@@ -0,0 +1,9 @@
+// src/routes/analyticsRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const analyticsController = require('../controllers/analyticsController');
+
+router.get('/usage', analyticsController.getUsageAnalytics);
+
+module.exports = router;
