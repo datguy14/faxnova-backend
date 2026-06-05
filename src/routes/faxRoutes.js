49add1bb8a095bbfb5c8c97ba380fa4782ad04f7
datguy14 <locklearcharles000@gmail.com>
2026-06-05 10:46:41 -0400

    Update faxRoutes.js

--- src/routes/faxRoutes.js
+++ src/routes/faxRoutes.js
@@ -1,25 +1,13 @@
-const express = require("express");
+
+import express from "express";
+import { sendFax, getFaxStatus } from "../controllers/faxController.js";
+
 const router = express.Router();
-const rateLimit = require("express-rate-limit");
 
-const agentAuth = require("../middleware/agentAuth");
-const faxController = require("../controllers/faxController");
+// POST /fax/send
+router.post("/send", sendFax);
 
-// Protect all routes
-router.use(agentAuth);
+// GET /fax/:provider/:faxId/status
+router.get("/:provider/:faxId/status", getFaxStatus);
 
-// Rate limiter
-const faxSendLimiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 30,
-  keyGenerator: (req) =>
-    req.headers["x-forwarded-for"]?.split(",")[0] || req.ip,
-});
-
-// SEND FAX
-router.post("/send", faxSendLimiter, faxController.sendFax);
-
-// GET FAX STATUS
-router.get("/:id/status", faxController.getFaxStatus);
-
-module.exports = router;
+export default router;
