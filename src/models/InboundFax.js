a0528eb132057af3c31a4b4db089db2c9776991c
datguy14 <locklearcharles000@gmail.com>
2026-06-26 09:23:37 -0400

    Update InboundFax.js

--- src/models/InboundFax.js
+++ src/models/InboundFax.js
@@ -1,5 +1,6 @@
 // src/models/InboundFax.js
-import mongoose from "mongoose";
+
+const mongoose = require("mongoose");
 
 const InboundFaxSchema = new mongoose.Schema(
   {
@@ -8,42 +9,55 @@ const InboundFaxSchema = new mongoose.Schema(
       required: true,
       index: true
     },
+
     provider: {
       type: String,
-      required: true
+      required: true,
+      index: true
     },
+
     fromNumber: {
       type: String,
       required: true
     },
+
     toNumber: {
       type: String,
-      required: true
+      required: true,
+      index: true
     },
+
     pages: {
       type: Number,
       default: 1
     },
+
     mediaUrl: {
       type: String,
       required: true
     },
+
     residencyZone: {
       type: String,
-      required: true
+      required: true,
+      index: true
     },
+
     sovereignty: {
       type: String,
       required: true
     },
+
     tenantId: {
       type: String,
-      default: null,
+      required: true,
       index: true
     },
+
     receivedAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
+      index: true
     }
   },
   {
@@ -51,4 +65,14 @@ const InboundFaxSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("InboundFax", InboundFaxSchema);
+// -----------------------------
+// Indexes for analytics + speed
+// -----------------------------
+
+InboundFaxSchema.index({ tenantId: 1, receivedAt: -1 });
+InboundFaxSchema.index({ provider: 1, receivedAt: -1 });
+InboundFaxSchema.index({ residencyZone: 1, receivedAt: -1 });
+InboundFaxSchema.index({ faxId: 1 });
+InboundFaxSchema.index({ toNumber: 1 });
+
+module.exports = mongoose.model("InboundFax", InboundFaxSchema);
