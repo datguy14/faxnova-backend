cc1be3048044f466a0249d565438f6f54b9f4889
datguy14 <locklearcharles000@gmail.com>
2026-06-26 09:06:18 -0400

    Update app.js (#243)

--- src/app.js
+++ src/app.js
@@ -53,3 +53,15 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+// Global error handler (place at bottom of src/app.js)
+
+app.use((err, req, res, next) => {
+  console.error("FaxNova Error:", err);
+
+  res.status(500).json({
+    error: err.message,
+    provider: err.provider || null,
+    code: err.code || "UNKNOWN_ERROR",
+    details: err.details || null
+  });
+});
