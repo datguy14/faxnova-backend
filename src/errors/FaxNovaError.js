e404eadba71dccc8d14560360c846f8d40653645
datguy14 <locklearcharles000@gmail.com>
2026-06-26 08:02:43 -0400

    Create FaxNovaError.js

---
+++ src/errors/FaxNovaError.js
@@ -0,0 +1,11 @@
+class FaxNovaError extends Error {
+  constructor(message, { provider = null, code = null, details = null } = {}) {
+    super(message);
+    this.name = "FaxNovaError";
+    this.provider = provider;
+    this.code = code;
+    this.details = details;
+  }
+}
+
+module.exports = FaxNovaError;
