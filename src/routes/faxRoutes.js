f8b3038059d4f1db7e92b71e19a0b85a4438c58c
datguy14 <locklearcharles000@gmail.com>
2026-06-26 08:35:47 -0400

    Update faxRoutes.js (#228)

--- src/routes/faxRoutes.js
+++ src/routes/faxRoutes.js
@@ -1,31 +1,5 @@
-// src/routes/faxRoutes.js
-
-const express = require("express");
-const router = express.Router();
-
-const faxController = require("../controllers/faxController");
-const residencyGuard = require("../middleware/residencyGuard");
-
-// POST /fax/send
-// Validates input via Zod in controller
-router.post(
-  "/send",
-  residencyGuard(), // Residency-aware if needed later
-  faxController.sendFax
-);
-
-// GET /fax/:id
-router.get(
-  "/:id",
-  residencyGuard(),
-  faxController.getFaxById
-);
-
-// POST /fax/:id/retry
 router.post(
   "/:id/retry",
   residencyGuard(),
   faxController.retryFax
 );
-
-module.exports = router;
