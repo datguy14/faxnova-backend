68a90af00c7286f807ba089f194281acfddc14dd
datguy14 <locklearcharles000@gmail.com>
2026-06-27 13:00:19 -0400

    Update AdminUser.js

--- src/models/AdminUser.js
+++ src/models/AdminUser.js
@@ -1,10 +1,8 @@
-// src/models/AdminUser.js
-
 const mongoose = require("mongoose");
 
 const AdminUserSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   passwordHash: { type: String, required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("AdminUser", AdminUserSchema);
