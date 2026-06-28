496509edef38cb55e72c07104aaaec5525af4beb
datguy14 <locklearcharles000@gmail.com>
2026-06-28 08:47:58 -0400

    Delete src/analyticsRoutes.js (#339)

--- src/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/analyticsRoutes.js
-
-const express = require('express');
-const router = express.Router();
-const analyticsController = require('../controllers/analyticsController');
-
-router.get('/usage', analyticsController.getUsageAnalytics);
-
-module.exports = router;
