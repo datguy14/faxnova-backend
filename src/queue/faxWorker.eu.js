a02d9b8b95a9e033b8c3ab2301ddce7b777c812e
datguy14 <locklearcharles000@gmail.com>
2026-06-28 14:41:18 -0400

    Create faxWorker.eu.js (#363)

---
+++ src/queue/faxWorker.eu.js
@@ -0,0 +1,37 @@
+// src/queue/faxWorker.eu.js
+
+const { Worker } = require("bullmq");
+const Redis = require("ioredis");
+const { sendFax } = require("../services/sendFaxService");
+const FaxNovaError = require("../errors/FaxNovaError");
+
+const connection = new Redis({
+  host: process.env.REDIS_HOST_EU,
+  port: process.env.REDIS_PORT_EU,
+  password: process.env.REDIS_PASSWORD_EU
+});
+
+const faxWorkerEU = new Worker(
+  "outboundFaxQueue",
+  async (job) => {
+    const { region } = job.data;
+
+    // Only process EU-region jobs
+    if (region !== "eu") {
+      return;
+    }
+
+    try {
+      const { tenantId, to, from, pages, documentUrl, tier } = job.data;
+      return await sendFax({ tenantId, to, from, pages, documentUrl, tier });
+    } catch (err) {
+      throw new FaxNovaError("EU fax worker failed", {
+        code: "FAX_WORKER_EU_FAILED",
+        details: err.message
+      });
+    }
+  },
+  { connection }
+);
+
+module.exports = faxWorkerEU;
