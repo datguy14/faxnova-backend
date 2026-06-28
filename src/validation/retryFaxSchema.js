46a0b0c84bc45654d849e100b2e097bf72d95d4e
datguy14 <locklearcharles000@gmail.com>
2026-06-28 05:00:31 -0400

    Update retryFaxSchema.js

--- src/validation/retryFaxSchema.js
+++ src/validation/retryFaxSchema.js
@@ -2,8 +2,21 @@
 
 const { z } = require("zod");
 
+/**
+ * Retry Fax Zod Schema (FaxNova v1)
+ *
+ * Validates:
+ * - faxId (MongoDB ObjectId string)
+ */
+
 const retryFaxSchema = z.object({
-  faxId: z.string().min(1, "faxId is required")
+  faxId: z
+    .string({
+      required_error: "faxId is required"
+    })
+    .regex(/^[a-f\d]{24}$/i, "Invalid faxId format")
 });
 
-module.exports = { retryFaxSchema };
+module.exports = {
+  retryFaxSchema
+};
