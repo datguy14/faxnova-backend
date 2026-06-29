5feead377d5b72783ef33511d319ce9d73e3e38e
datguy14 <locklearcharles000@gmail.com>
2026-06-29 04:57:22 -0400

    Delete src/models/AdminUser.js (#429)

--- src/models/AdminUser.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const AdminUserSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  passwordHash: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("AdminUser", AdminUserSchema);
