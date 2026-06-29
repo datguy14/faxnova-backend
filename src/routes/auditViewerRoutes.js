063615bd11531622483be16ebe83824c795be644
datguy14 <locklearcharles000@gmail.com>
2026-06-29 04:22:29 -0400

    Delete src/routes/auditViewerRoutes.js (#410)

--- src/routes/auditViewerRoutes.js
+++
@@ -1,8 +0,0 @@
-// src/routes/auditViewerRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getAuditLogs } = require('../controllers/auditViewerController');
-
-router.get('/audit/logs', getAuditLogs);
-
-module.exports = router;
