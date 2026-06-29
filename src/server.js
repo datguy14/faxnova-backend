56e51e8603026e2463b231b873fa775ea0a2702d
datguy14 <locklearcharles000@gmail.com>
2026-06-28 20:05:58 -0400

    Update server.js (#365)

--- src/server.js
+++ src/server.js
@@ -1,13 +1,29 @@
 // src/server.js
-import dotenv from "dotenv";
-import app from "./app.js";
-import connectDB from "./config/db.js";
 
-dotenv.config();
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
 
-// Connect to database
+// ---------------------------
+// Database Connection
+// ---------------------------
 connectDB();
 
+// ---------------------------
+// Start Multi‑Region Workers
+// ---------------------------
+
+// US region workers
+require("./queue/faxWorker.us");
+require("./queue/retryWorker.us");
+
+// EU region workers
+require("./queue/faxWorker.eu");
+require("./queue/retryWorker.eu");
+
+// ---------------------------
+// Server Startup
+// ---------------------------
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
