f616b5a458226a04f9725935e24b5eed60736880
datguy14 <locklearcharles000@gmail.com>
2026-06-29 04:23:53 -0400

    Delete src/routes/faxEventHistoryRoutes.js (#411)

--- src/routes/faxEventHistoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { getFaxEventHistory } = require('../controllers/faxEventHistoryController');
-
-// GET /fax/history/:id — protected
-router.get('/history/:id', auth, getFaxEventHistory);
-
-module.exports = router;
