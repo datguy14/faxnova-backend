304a5417328730405d327573a885335ab972c5b4
datguy14 <locklearcharles000@gmail.com>
2026-06-29 04:21:25 -0400

    Delete src/routes/faxDownloadRoutes.js (#409)

--- src/routes/faxDownloadRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/faxDownloadRoutes.js
-
-const express = require('express');
-const router = express.Router();
-const { downloadFaxController } = require('../controllers/faxDownloadController');
-
-router.get('/:faxId/download', downloadFaxController);
-
-module.exports = router;
