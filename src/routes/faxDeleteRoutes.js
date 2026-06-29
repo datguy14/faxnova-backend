6bd58c5399fde4f9944873b29ec30b83ea0a3b5b
datguy14 <locklearcharles000@gmail.com>
2026-06-29 04:19:48 -0400

    Delete src/routes/faxDeleteRoutes.js (#408)

--- src/routes/faxDeleteRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/faxDeleteRoutes.js
-
-const express = require('express');
-const router = express.Router();
-const { deleteFaxController } = require('../controllers/faxDeleteController');
-
-router.delete('/:faxId', deleteFaxController);
-
-module.exports = router;
