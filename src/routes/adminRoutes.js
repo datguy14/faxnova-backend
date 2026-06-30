93a320326f9723a3032664390829812d68c15f99
datguy14 <locklearcharles000@gmail.com>
2026-06-30 19:43:11 -0400

    Delete src/routes/adminRoutes.js

--- src/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { auth, requireAdmin } = require("../middleware/authMiddleware");
-
-router.get("/dashboard", auth, requireAdmin, async (req, res) => {
-  res.json({ message: "Admin dashboard", user: req.user });
-});
-
-module.exports = router;
