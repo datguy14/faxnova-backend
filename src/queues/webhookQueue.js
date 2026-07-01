8c01b0f82f9bacc8951519563f574e47fa660db2
datguy14 <locklearcharles000@gmail.com>
2026-07-01 00:42:26 -0400

    Update webhookQueue.js (#504)

--- src/queues/webhookQueue.js
+++ src/queues/webhookQueue.js
@@ -3,4 +3,15 @@
 const { Queue } = require("bullmq");
 const connection = require("../lib/redis");
 
-module.exports = new Queue("webhookQueue", { connection });
+module.exports = new Queue("webhookQueue", {
+  connection,
+  defaultJobOptions: {
+    attempts: 5, // max retries
+    backoff: {
+      type: "exponential",
+      delay: 500, // base delay
+    },
+    removeOnComplete: true,
+    removeOnFail: false, // keep failures for DLQ processing
+  },
+});
