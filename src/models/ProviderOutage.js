63060d39fba2f688c1ade189abb3b7a88e02b3ce
datguy14 <locklearcharles000@gmail.com>
2026-07-01 04:09:57 -0400

    Create ProviderOutage.js (#531)

---
+++ src/models/ProviderOutage.js
@@ -0,0 +1,48 @@
+// src/models/ProviderOutage.js
+
+const mongoose = require("mongoose");
+
+const ProviderOutageSchema = new mongoose.Schema({
+  provider: {
+    type: String,
+    index: true,
+  },
+
+  region: {
+    type: String,
+    index: true,
+  },
+
+  outageType: {
+    type: String,
+    enum: ["partial", "full", "degraded"],
+    required: true,
+  },
+
+  detectedAt: {
+    type: Date,
+    default: Date.now,
+    index: true,
+  },
+
+  resolvedAt: {
+    type: Date,
+    index: true,
+  },
+
+  details: {
+    type: Object,
+    default: {},
+  },
+
+  attemptsMade: {
+    type: Number,
+    default: 0,
+  },
+});
+
+// Composite indexes
+ProviderOutageSchema.index({ provider: 1, region: 1 });
+ProviderOutageSchema.index({ outageType: 1, detectedAt: -1 });
+
+module.exports = mongoose.model("ProviderOutage", ProviderOutageSchema);
