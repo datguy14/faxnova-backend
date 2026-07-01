42e36651217ae5057b6958b825037ab1c26710bc
datguy14 <locklearcharles000@gmail.com>
2026-07-01 15:13:10 -0400

    Update faxController.js

--- src/controllers/faxController.js
+++ src/controllers/faxController.js
@@ -1,103 +1,17 @@
 // src/controllers/faxController.js
 
-const OutboundFax = require("../models/OutboundFax");
-const outboundFaxQueue = require("../queue/outboundFaxQueue");
 const FaxNovaError = require("../errors/FaxNovaError");
+const { createFaxSchema } = require("../schemas/faxSchemas");
+const faxService = require("../services/faxService");
 
-async function sendFax(req, res) {
+exports.createFax = async (req, res, next) => {
   try {
-    const { tenantId, to, from, pages, documentUrl, region, tier } = req.body;
+    const data = createFaxSchema.parse(req.body);
 
-    if (!tenantId || !to || !from || !pages || !documentUrl || !region) {
-      throw new FaxNovaError("Missing required fields", {
-        code: "FAX_VALIDATION_ERROR"
-      });
-    }
-
-    // Create async fax record
-    const faxRecord = await OutboundFax.create({
-      tenantId,
-      to,
-      from,
-      pages,
-      documentUrl,
-      region,
-      tier,
-      status: "queued",
-      createdAt: new Date()
-    });
-
-    // Push job into outbound queue
-    await outboundFaxQueue.add(
-      "sendFax",
-      {
-        faxId: faxRecord.faxId,
-        tenantId,
-        to,
-        from,
-        pages,
-        documentUrl,
-        region,
-        tier
-      },
-      {
-        attempts: 3,
-        backoff: { type: "exponential", delay: 30000 }
-      }
-    );
-
-    return res.json({
-      success: true,
-      faxId: faxRecord.faxId,
-      message: "Fax queued for async processing"
-    });
+    const fax = await faxService.createFaxJob(data);
+    res.json(fax);
 
   } catch (err) {
-    return res.status(400).json({
-      success: false,
-      error: err.message
-    });
+    next(new FaxNovaError(err.errors?.[0]?.message || "Invalid fax payload", 400));
   }
-}
-
-async function getFaxById(req, res) {
-  try {
-    const { faxId } = req.params;
-
-    const fax = await OutboundFax.findOne({ faxId });
-    if (!fax) {
-      throw new FaxNovaError("Fax not found", {
-        code: "FAX_NOT_FOUND",
-        faxId
-      });
-    }
-
-    return res.json({ success: true, fax });
-  } catch (err) {
-    return res.status(404).json({
-      success: false,
-      error: err.message
-    });
-  }
-}
-
-async function listFaxes(req, res) {
-  try {
-    const { tenantId } = req.params;
-
-    const faxes = await OutboundFax.find({ tenantId }).sort({ createdAt: -1 });
-
-    return res.json({ success: true, faxes });
-  } catch (err) {
-    return res.status(400).json({
-      success: false,
-      error: err.message
-    });
-  }
-}
-
-module.exports = {
-  sendFax,
-  getFaxById,
-  listFaxes
 };
