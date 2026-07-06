fbb07c863def05a29c1bb160077188201ec28070
Charles Locklear <locklearcharles000@gmail.com>
2026-07-06 00:31:27 -0400

    Update retryFaxService.js

--- src/services/retryFaxService.js
+++ src/services/retryFaxService.js
@@ -1,45 +1,10 @@
 // src/services/retryFaxService.js
 
-const ProviderRouter = require("./providerRouter.v2");
-const telnyxAdapter = require("../providers/telnyxAdapter");
-const sinchAdapter = require("../providers/sinchAdapter");
+const { Queue } = require("bullmq");
+const { connection } = require("../lib/redis");
 
-const FaxEventService = require("./FaxEventService");
-const OutboundFax = require("../models/OutboundFax");
+const outboundFaxQueue = new Queue("outboundFaxQueue", { connection });
 
-class RetryFaxService {
-  static async retry({ faxId }) {
-    const fax = await OutboundFax.findById(faxId);
-    if (!fax) throw new Error("Outbound fax not found");
-
-    const provider = ProviderRouter.pickOutboundProvider(fax.provider);
-
-    const adapter =
-      provider === "telnyx" ? telnyxAdapter : sinchAdapter;
-
-    const result = await adapter.sendFax({
-      to: fax.to,
-      storageKey: fax.storageKey,
-      region: fax.region
-    });
-
-    if (!result.ok) {
-      throw new Error(`Retry failed: ${result.error}`);
-    }
-
-    fax.providerFaxId = result.providerFaxId;
-    await fax.save();
-
-    await FaxEventService.recordOutbound({
-      provider,
-      providerFaxId: result.providerFaxId,
-      to: fax.to,
-      storageKey: fax.storageKey,
-      region: fax.region
-    });
-
-    return { ok: true, providerFaxId: result.providerFaxId };
-  }
-}
-
-module.exports = RetryFaxService;
+exports.enqueueInitialSend = async fax => {
+  await outboundFaxQueue.add("sendFax", { faxId: fax._id.toString() });
+};
