02e2a203492c55967246c24e5941dcd435171421
Charles Locklear <locklearcharles000@gmail.com>
2026-07-06 02:17:08 -0400

    Update sinchAdapter.js (#820)

--- src/providers/sinchAdapter.js
+++ src/providers/sinchAdapter.js
@@ -1,59 +1,31 @@
 // src/providers/sinchAdapter.js
 
 const axios = require("axios");
+const OutboundFax = require("../models/OutboundFax");
 
-const SINCH_BASE_URL = "https://fax.api.sinch.com/v1";
-
-exports.sendFax = async ({ to, storageKey, region }) => {
-  try {
-    const response = await axios.post(
-      `${SINCH_BASE_URL}/outbound/faxes`,
-      {
-        to,
-        mediaUrl: storageKey,
-        region
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.SINCH_API_KEY}`,
-          "Content-Type": "application/json"
-        }
+exports.sendFax = async fax => {
+  const response = await axios.post(
+    "https://api.sinch.com/fax/v1/send",
+    {
+      to: fax.to,
+      document: fax.storageKey
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.SINCH_API_KEY}`
       }
-    );
+    }
+  );
 
-    return {
-      ok: true,
-      providerFaxId: response.data.id,
-      raw: response.data
-    };
-  } catch (err) {
-    return {
-      ok: false,
-      error: err.response?.data?.message || err.message
-    };
-  }
-};
+  const providerFaxId = response.data.id;
 
-exports.getFaxStatus = async (providerFaxId) => {
-  try {
-    const response = await axios.get(
-      `${SINCH_BASE_URL}/outbound/faxes/${providerFaxId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.SINCH_API_KEY}`
-        }
-      }
-    );
+  fax.providerFaxId = providerFaxId;
+  fax.status = "processing";
+  await fax.save();
 
-    return {
-      ok: true,
-      status: response.data.status,
-      raw: response.data
-    };
-  } catch (err) {
-    return {
-      ok: false,
-      error: err.response?.data?.message || err.message
-    };
-  }
+  return {
+    provider: "sinch",
+    providerFaxId,
+    status: "processing"
+  };
 };
