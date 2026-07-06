5e65a94f47984fb7e8b9027ea951f7612ea7f2cf
Charles Locklear <locklearcharles000@gmail.com>
2026-07-06 00:37:07 -0400

    Update inboundFaxController.js (#811)

--- src/controllers/inboundFaxController.js
+++ src/controllers/inboundFaxController.js
@@ -1,18 +1,10 @@
 // src/controllers/inboundFaxController.js
 
-const InboundFax = require("../models/InboundFax");
+const inboundFaxService = require("../services/inboundFaxService");
 
 exports.receiveInboundFax = async (req, res) => {
   try {
-    const fax = await InboundFax.create({
-      from: req.body.from,
-      provider: req.body.provider,
-      providerFaxId: req.body.providerFaxId,
-      storageKey: req.body.storageKey,
-      region: req.body.region,
-      status: req.body.status || "received"
-    });
-
+    const fax = await inboundFaxService.processInboundFax(req.body);
     res.json(fax);
   } catch (err) {
     res.status(500).json({ error: err.message });
