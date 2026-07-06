9a6d30cbb0b98b6f87d4131788276dc385fb2dc8
Charles Locklear <locklearcharles000@gmail.com>
2026-07-06 00:26:46 -0400

    Update faxService.js

--- src/services/faxService.js
+++ src/services/faxService.js
@@ -1,27 +1,7 @@
-// src/services/faxService.js — Strict‑Mode CommonJS Version
+// src/services/faxService.js
 
-const OutboundFax = require("../models/OutboundFax");
+const outboundFaxService = require("./outboundFaxService");
 
-exports.createOutboundFax = async data => {
-  return await OutboundFax.create({
-    to: data.to,
-    provider: data.provider,
-    storageKey: data.storageKey,
-    region: data.region
-  });
-};
-
-exports.getOutboundFaxById = async faxId => {
-  return await OutboundFax.findById(faxId);
-};
-
-exports.getOutboundFaxByProviderId = async providerFaxId => {
-  return await OutboundFax.findOne({ providerFaxId });
-};
-
-exports.updateOutboundFaxStatus = async (providerFaxId, status) => {
-  return await OutboundFax.updateOne(
-    { providerFaxId },
-    { status }
-  );
+exports.createOutboundFax = async payload => {
+  return outboundFaxService.processOutboundFax(payload);
 };
