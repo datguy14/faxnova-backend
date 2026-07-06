2637bf6f097879b415b31c3cdd2940528e34366d
Charles Locklear <locklearcharles000@gmail.com>
2026-07-06 02:35:35 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,22 @@
 // src/server.js
 
+require("dotenv").config();
+
+const app = require("./app");
+const { connectMongo } = require("./lib/mongo");
+require("./lib/redis");
+
+// Boot workers
 require("./workers/outboundFaxWorker");
 require("./workers/retryFaxWorker");
 require("./workers/webhookWorker");
+
+// Start server
+const PORT = process.env.PORT || 3000;
+
+(async () => {
+  await connectMongo();
+  app.listen(PORT, () => {
+    console.log(`FaxNova backend running on port ${PORT}`);
+  });
+})();
