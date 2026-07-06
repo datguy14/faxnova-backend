e2eb92325b5e58a0fc7430df133312b865c4bf70
Charles Locklear <locklearcharles000@gmail.com>
2026-07-06 01:59:20 -0400

    Update outboundFaxWorker.js (#815)

--- src/workers/outboundFaxWorker.js
+++ src/workers/outboundFaxWorker.js
@@ -1,1 +1,14 @@
+// src/workers/outboundFaxWorker.js
 
+const { Worker } = require("bullmq");
+const { connection } = require("../lib/redis");
+const sendFaxService = require("../services/sendFaxService");
+
+module.exports = new Worker(
+  "outboundFaxQueue",
+  async job => {
+    const { faxId } = job.data;
+    await sendFaxService.sendFax(faxId);
+  },
+  { connection }
+);
